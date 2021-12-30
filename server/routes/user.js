6b40c60cd8cfa7f493eaf74e00c9b7decb043450
Victor Tran <vtranwd@gmail.com>
2021-12-29 19:01:35 -0800

    cleaning up

--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express';
-import { getUsers, createUser } from '../controllers/user.js'
-import user from '../models/user.js';
-const router = express.Router();
-
-router.get('/', getUsers);
-router.post('/', createUser);
-
-export default router;
