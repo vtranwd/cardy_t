99ea986127224658bc2b9d0e4c16c63ebe9190ef
Victor Tran <vtranwd@gmail.com>
2021-12-30 19:00:19 -0800

    new demo material

---
+++ cardy_t/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">Bobby</a>
+                    <div className="metadata">
+                        <span className="date">Today at 5:00PM</span>
+                    </div>
+                    <div className="text">Hey that's cool!</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
